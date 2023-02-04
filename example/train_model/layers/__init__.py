7b9c890b0b334a71ac1b2df0a2f543f5dc0548c7
lsjsj92 <lsjsj92@naver.com>
2023-02-04 09:12:45 +0900

    Add train layers

---
+++ example/train_model/layers/__init__.py
@@ -0,0 +1,1 @@
+from layers.layer import *
---
+++ example/train_model/layers/layer.py
@@ -0,0 +1,89 @@
+
+import numpy as np
+import torch
+
+
+class FeaturesLinear(torch.nn.Module):
+
+    def __init__(self, field_dims, output_dim=1):
+        super().__init__()
+        self.fc = torch.nn.Embedding(sum(field_dims), output_dim)
+        self.bias = torch.nn.Parameter(torch.zeros((output_dim,)))
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+
+    def forward(self, x):
+        # x.new_tensor 하면 offset 위치만큼 index 값이 들어가게 됨
+        # 거기서 x와 +를 하니까 해당 feature의 위치의 embedding 값을 표현할 수 있음
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+
+        # fc에 넣으면 embedding dim이 1차원임
+        # 즉, 각 feature에 대한 embedding 위치에 대한 값 1차원의 값을 가지고 오는 것
+
+        return torch.sum(self.fc(x), dim=1) + self.bias
+
+
+class UIEmbedding(torch.nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        torch.nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+
+        return self.embedding(x)
+
+
+class FeaturesEmbedding(torch.nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        torch.nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+
+        return self.embedding(x)
+
+
+class FactorizationMachine(torch.nn.Module):
+
+    def __init__(self, reduce_sum=True):
+        super().__init__()
+        self.reduce_sum = reduce_sum
+
+    def forward(self, x):
+        square_of_sum = torch.sum(x, dim=1) ** 2
+        sum_of_square = torch.sum(x ** 2, dim=1)
+
+        ix = square_of_sum - sum_of_square
+
+        if self.reduce_sum:
+            ix = torch.sum(ix, dim=1, keepdim=True)
+
+        return 0.5 * ix
+
+
+class MultiLayerPerceptron(torch.nn.Module):
+
+    def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
+        super().__init__()
+        layers = list()
+        for embed_dim in embed_dims:
+            layers.append(torch.nn.Linear(input_dim, embed_dim))
+            layers.append(torch.nn.BatchNorm1d(embed_dim))
+            layers.append(torch.nn.ReLU())
+            layers.append(torch.nn.Dropout(p=dropout))
+            input_dim = embed_dim
+        if output_layer:
+            layers.append(torch.nn.Linear(input_dim, 1))
+        self.mlp = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.mlp(x)
